91923eb66c77b15c20fbb32c21f87e496927f43e
Emelie Ilola <emelie.ilola@outlook.com>
2026-03-30 10:53:30 +0200

    jippi

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Ändring i filen");
+        System.out.println("jippi");
     }
 }

0382128f5252feb65a8169bd3293fedb767e3434
Emelie Ilola <emelie.ilola@outlook.com>
2026-03-30 09:42:21 +0200

    ändra2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Ändring i filen");
+        System.out.println("Ändring2");
     }
+
 }

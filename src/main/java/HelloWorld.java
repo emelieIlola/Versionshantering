aa2f514ab2a828f102344f82e89d2db630aa110f
Emelie Ilola <emelie.ilola@outlook.com>
2026-04-01 13:26:02 +0200

    aprilapril

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,9 +7,9 @@ public class HelloWorld {
         System.out.println("Hej hej från Sanna!");
 
         String dag = "1 april";
-if (dag.equals("1 april")){
-    System.out.println("April, april din dumma sill, jag kan lura dig vart jag vill");
-}
-        System.out.println();
+        if (dag.equals("1 april")) {
+            System.out.println("April, april din dumma sill, jag kan lura dig vart jag vill");
+        }
+
     }
 }
